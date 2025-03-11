0f8172b735f39f31612a3ae93876da420bea8aef
DTM1218 <dtm121898@gmail.com>
2025-03-11 04:45:13 -0700

    LoopyJakeMcPow: Fixed visual effects for Round Glasses

--- mods-unpacked/dtm-LoopyJakeMcPow/item_round_glasses.gd
+++ mods-unpacked/dtm-LoopyJakeMcPow/item_round_glasses.gd
@@ -9,20 +9,30 @@ func on_load(_item: Item) -> void:
 	setup()
 
 func setup() -> void:
-	BattleService.s_battle_started.connect(on_battle_start)
+	BattleService.s_toon_dealt_damage.connect(toon_dealt_damage)
 
-func on_battle_start(manager: BattleManager) -> void:
-	manager.s_action_finished.connect(action_finished)
+func toon_dealt_damage(action: BattleAction, target: Node3D, amount: int):
+	if action is not GagDrop and amount > 0:
+		var manager := BattleService.ongoing_battle
+		var shock : StatEffectAftershock = find_cog_aftershock(manager, target)
+		if shock == null:
+			return
+		
+		await manager.sleep(1.0)
+		target.stats.hp -= shock.amount
+		manager.battle_text(target, "-"+str(shock.amount), BattleText.colors.orange[0], BattleText.colors.orange[1])
+		Util.get_player().boost_queue.queue_text("Shocking!", Color(0.208, 0.957, 1.0))
+		target.set_animation('pie-small')
+		await manager.sleep(2.0)
+		
+		if shock.rounds == 0:
+			await BattleService.ongoing_battle.expire_status_effect(shock)
+		elif shock.rounds > 0:
+			shock.rounds -= 1
 
-func action_finished(action : BattleAction):
-	if action is ToonAttack and action is not GagDrop and action.damage > 0:
-		for target in action.targets:
-			for effect: StatusEffect in BattleService.ongoing_battle.get_statuses_for_target(target):
-				if effect is not StatEffectAftershock:
-					continue
-				var shock : StatEffectAftershock = effect
-				await shock.renew()
-				if shock.rounds == 0:
-					await BattleService.ongoing_battle.expire_status_effect(shock)
-				elif shock.rounds > 0:
-					shock.rounds -= 1
+func find_cog_aftershock(manager: BattleManager, cog: Cog) -> StatEffectAftershock:
+	for effect in manager.status_effects:
+		if effect is StatEffectAftershock:
+			if cog == effect.target:
+				return effect
+	return null
